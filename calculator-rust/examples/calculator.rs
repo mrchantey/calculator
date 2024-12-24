0d36d2bba5b8e186150383cf3ddc7ae8613ea289
Peter Hayman <peteyhayman@gmail.com>
2024-12-25 08:46:47 +1100

    feat: wasm build

---
+++ calculator-rust/examples/calculator.rs
@@ -0,0 +1,8 @@
+#[allow(unused_imports)]
+use calculator_rust::*;
+// use wasm_bindgen::prelude::wasm_bindgen;
+
+pub fn main() {
+    let _a = 1+2;
+    // greet("it loaded!");
+}
---
+++ calculator-rust/src/lib.rs
@@ -0,0 +1,22 @@
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern "C" {
+    pub fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet(name: &str) {
+    alert(&format!("Hello, {}!", name));
+}
+#[wasm_bindgen]
+/// Does something cool
+pub fn greet_many(name: &str, name2: &str) {
+    alert(&format!("Hello, {} and {}!", name, name2));
+}
+
+#[wasm_bindgen]
+pub fn foo(a: u32, b: u32, c: u32) -> u32 {
+    a + b + c
+    // alert(&format!("Hello, {}!", name));
+}
